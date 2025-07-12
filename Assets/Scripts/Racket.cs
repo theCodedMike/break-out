7ff7bd9adcc0e8a294db5ef905e9ce7ee0627c06
lixia <lixiaofeng1115@outlook.com>
2025-07-12 11:36:49 +0800

    场景搭建、横板移动

---
+++ Assets/Scripts/Racket.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Racket : MonoBehaviour
+{
+    [Header("横板移动速度")]
+    public float speed = 10f;
+
+    private const float BorderWidth = 5.2f;
+    
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (transform.position.x > -BorderWidth)
+                transform.Translate(Vector3.left * (Time.deltaTime * speed));
+        } else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if(transform.position.x < BorderWidth)
+                transform.Translate(Vector3.right * (Time.deltaTime * speed));
+        }
+    }
+}
